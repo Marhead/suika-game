404e2f399cb4dd6aa803047fa84453ef42358fab
Marhead <marhead@DESKTOP-UCEMNJN>
2023-11-28 13:49:00 +0900

    checking thumbnail image

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: '동물합치기',
   description: '동물을 합쳐서 돼지를 만들자',
+  icons: 'animal_3d/07_Pig.png'
 }
 
 export default function RootLayout({
