974a88941a5d81b4d8b2d69d4755867f6eea145b
Marhead <marhead@DESKTOP-UCEMNJN>
2023-12-10 15:41:06 +0900

    working in progress

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,22 +1,21 @@
-import React, { ReactNode } from "react";
-import ReactDOM from "react-dom";
 
 interface ModalProps {
     open: boolean;
-    onClose: () => void;
-    children: ReactNode;
 }
 
-const Modal = ({ open, onClose, children }: ModalProps) => {
+const Modal = ({open}: ModalProps) => {
+    const onClose = () => {
+        open = !!open;
+    }
+
     if (!open) return null;
-    return ReactDOM.createPortal(
-        <>
-            <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-40 flex justify-center items-center" />
-            <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-12 bg-white z-5">
-                <button onClick={onClose}>모달 닫기</button>
-                {children}
-            </div>
-        </>,
-        document.getElementById("global-modal") as HTMLElement
+
+    return (
+        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-40 flex justify-center items-center">
+            This is modal
+            <button onClick={onClose}>모달닫기</button>
+        </div>
     );
-};
+}
+
+export default Modal;
